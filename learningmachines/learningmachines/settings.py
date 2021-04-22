b58fa864726a4a97af81007c5e62ee30294d6c90
srivasyi <srivasyi.mail.uc.edu>
2021-04-22 23:02:41 +0000

    Testing - Beginning

--- learningmachines/learningmachines/settings.py
+++ learningmachines/learningmachines/settings.py
@@ -134,12 +134,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_HOST = 'smtp.sendgrid.net'
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testsite_app'
-EMAIL_HOST_PASSWORD = 'mys3cr3tp4ssw0rd'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'TestSite Team <noreply@example.com>'
+EMAIL_HOST_USER = 'machinelearning.dsc@gmail.com'
+EMAIL_HOST_PASSWORD = '1234Yesgo'
+
+
 
 
 # Internationalization
--- learningmachines/learningmachines/urls.py
+++ learningmachines/learningmachines/urls.py
@@ -27,6 +27,7 @@ from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 
 
+
 def redirect_vis_view(request):
     qry_str = {k: v[0] for k, v in dict(request.GET).items()}
     print(qry_str)
@@ -71,24 +72,20 @@ urlpatterns = [
 
     path('', RedirectView.as_view(url='searcher', permanent=True)),
 
-    url(r'^reset/$',
-        auth_views.PasswordResetView.as_view(
-            template_name='password_reset.html',
-            email_template_name='password_reset_email.html',
-            subject_template_name='password_reset_subject.txt'
-        ),
-        name='password_reset'),
-    url(r'^reset/done/$',
-        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
-        name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
-        name='password_reset_confirm'),
-    url(r'^reset/complete/$',
-        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
-        name='password_reset_complete'),
 
-   
+    path('reset_password/', 
+        auth_views.PasswordResetView.as_view(template_name = "searcher/password_reset.html"), 
+        name = "reset_password"),
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name = "searcher/password_reset_sent.html"), 
+        name = "password_reset_done"),
+    path('reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name = "searcher/password_reset_form.html"), 
+        name = "password_reset_confirm"),
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name = "searcher/password_reset_complete.html"), 
+        name = "password_reset_complete"),
+  
 
 ]
 
