3534712346516447a69ab1bb198db9cd22909464
Shwetasahu2204 <34883836+Shwetasahu2204@users.noreply.github.com>
2021-06-16 16:34:19 -0400

    Update credentials.py

--- learningmachines/credentials.py
+++ learningmachines/credentials.py
@@ -1,1 +1,29 @@
+AWS_PROFILE = {
+    'ACCESS_KEY': 'xxxxx',
+    'SECRET_KEY': 'smishra',
+    'AWS_HOST': 'search-mellon-team-02-loxyxjaof7ypdgvufsulhomyuq.us-east-2.es.amazonaws.com'
+}
 
+
+DB_ENV = 'DEV' #'DEV', 'LOCAL', 'PRODUCTION'
+
+DEV_DB_PROFILE = {
+	'user' : 'sahusa',
+	'password' : 'Harsh@gate26'
+}
+DJANGO_SECRET = 'xxxxxx'
+
+SQS_QUEUE_NAME = 'learning-machines-dsc_sahusa'
+
+
+S3_OBJECT = {
+	'USE_S3' : True,
+	'BUCKET_NAME' : 'learningmachines-static',
+	'CUSTOM_DOMAIN' : f'learningmachines-static.s3.amazonaws.com'
+}
+
+
+EMAIL_INFO = {
+	'EMAIL_NAME' : 'shwetasahu22@gmail.com',
+	'EMAIL_PW' : 'asciicode'
+}
