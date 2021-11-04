e0b29e017e13f852b4e45f3d4201674c96ca5384
Ubuntu <ubuntu@ip-172-31-22-29.us-east-2.compute.internal>
2021-11-04 19:09:50 +0000

    adding new allowed host

--- learningmachines/learningmachines/settings.py
+++ learningmachines/learningmachines/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = DJANGO_SECRET
 
 DEBUG = not S3_OBJECT['USE_S3']
 
-ALLOWED_HOSTS = ['3.19.31.134', 'localhost', 'modelofmodels.io', 'themlmom.com', 'rnlp.themlmom.com']
+ALLOWED_HOSTS = ['3.19.31.134', 'localhost', 'modelofmodels.io', 'themlmom.com', 'rnlp.themlmom.com', '13.59.40.40']
 
 
 # Application definition
