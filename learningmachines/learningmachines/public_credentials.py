06ef78a151552c7e29696a4502dbf07a38c52d5e
Ezra Edgerton <ezraedgerton@MacBook-Pro-68.local>
2022-01-25 11:47:53 -0600

    updating aws secretsmanager handling in public_credentials

--- learningmachines/learningmachines/public_credentials.py
+++ learningmachines/learningmachines/public_credentials.py
@@ -1,11 +1,36 @@
 import os
+import boto3 
+import json 
+
+
 
 try:
 	from .credentials import AWS_PROFILE, DB_ENV, DEV_DB_PROFILE, DJANGO_SECRET, SQS_QUEUE_NAME, REDIS_IP, REDIS_URL, RDS_ENDPOINT, S3_OBJECT, EMAIL_INFO
 	print("credentials file found")
 
 except:
-	print("no credentials file found, searching system variables...")
+	#processing for docker deploy with aws secretsmanagerkey
+	if 'DJANGO_SECRETS' in os.environ:
+		print("django secrets found, setting system variables!")
+		client = boto3.client('secretsmanager', region_name='us-east-2')
+		response = client.get_secret_value(
+    		SecretId=os.environ['DJANGO_SECRETS']
+		)
+		database_secrets = json.loads(response['SecretString'])
+		#set variables
+		rds_password = database_secrets['RDS_PASSWORD']
+		django_secret = database_secrets['DJANGO_SECRET']
+		redis_ip = database_secrets['REDIS_IP']
+		client_email_pw = database_secrets['CLIENT_EMAIL_PW']
+	else:
+		print("no aws secret environment variables found, searching os files")
+		#if no secrets ID, look for them as set environment variables
+		rds_password = os.environ['RDS_PASSWORD']
+		django_secret = os.environ['DJANGO_SECRET']
+		redis_ip = os.environ['REDIS_IP']
+		client_email_pw = os.environ['CLIENT_EMAIL_PW']
+
+
 	AWS_PROFILE = {
 	    'ACCESS_KEY': '',
 	    'SECRET_KEY': '',
@@ -17,18 +42,18 @@ except:
 
 	DEV_DB_PROFILE = {
 		'user' : 'zhaowezra',#'zhaowezra',
-		'password' : os.environ['RDS_PASSWORD']
+		'password' : rds_password
 	}
 
 
-	DJANGO_SECRET = os.environ['DJANGO_SECRET']
+	DJANGO_SECRET = django_secret
 
 	RDS_ENDPOINT='mellon-db-01.cykdbek7llhv.us-east-2.rds.amazonaws.com'
 
 	SQS_QUEUE_NAME = 'learning-machines-'
 	#SQS_QUEUE_NAME = 'learning-machines-ez-local-'
 
-	REDIS_IP = os.environ['REDIS_IP']
+	REDIS_IP = redis_ip
 
 	REDIS_URL= 'redis://'+ REDIS_IP +':6379' # redis://'+ 'localhost' +':6379'
 
@@ -41,5 +66,5 @@ except:
 
 	EMAIL_INFO = {
 		'EMAIL_NAME' : 'ezraedgerton@gmail.com',
-		'EMAIL_PW' : os.environ['CLIENT_EMAIL_PW']
+		'EMAIL_PW' : client_email_pw
 	}
