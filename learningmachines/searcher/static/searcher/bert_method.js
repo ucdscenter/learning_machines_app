69d2be2648700192a627bb91181a8e612024ac05
Zhaowezra <edgertej@ucmail.uc.edu>
2022-02-16 10:20:44 -0600

    fix file load

--- learningmachines/searcher/static/searcher/bert_method.js
+++ learningmachines/searcher/static/searcher/bert_method.js
@@ -1,8 +1,7 @@
 'use-strict'
 
 async function wrapper(){
-    let data = await d3.json("/Users/happyhome/Documents/learning_machines_app/learningmachines/searcher/tempmodeldata/extra_json_points")
-
+    let data = await d3.json(static_url + 'sample_json_of_points.json')
     console.log(data)
 }
 
