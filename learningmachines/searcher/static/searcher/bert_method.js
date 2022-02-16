b8ccb8cb7a4a9e6cd9cfcfe69bf6307df939cb51
Zenith Zack <faldund@outlook.com>
2022-02-16 11:44:41 -0500

    made an svg canvas

--- learningmachines/searcher/static/searcher/bert_method.js
+++ learningmachines/searcher/static/searcher/bert_method.js
@@ -2,8 +2,19 @@
 
 async function wrapper(){
 
-    let data = await d3.json(static_url + 'sample_json_of_points.json')
-    console.log(data)
+    let data = await d3.json(static_url + 'extra_json_points.json')
+    console.log(data.x)
+
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    var svg = d3.select('#my_dataviz')
+        .append('svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append('g')
+            .attr("transform", "translate(" + margin.left +"," + margin.top + ")");
 }
 
 wrapper()  
