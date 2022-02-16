f0fdb2d64bb2b88793ee6c137abf1d3d4002d476
Zenith Zack <faldund@outlook.com>
2022-02-16 10:59:08 -0500

    pushed for ezra to check the error

--- learningmachines/searcher/static/searcher/bert_method.js
+++ learningmachines/searcher/static/searcher/bert_method.js
@@ -1,11 +1,13 @@
 'use-strict'
 
 async function wrapper(){
-    let data = await d3.json("/Users/happyhome/Documents/learning_machines_app/learningmachines/searcher/tempmodeldata/extra_json_points")
-
-    console.log(data)
+    // let data = await d3.json("https://github.com/ucdscenter/learning_machines_app/blob/bert_method/learningmachines/searcher/static/searcher/bert_method_data/extra_json_points.json")
+    var jsonData = JSON.parse(document.querySelector(".jsonData").getAttribute("data-json"));
+    var data = jsonData.map((item) => item.x)
+    console.log(data);
 }
+
+wrapper()  
 
 // d3.json("file.json").then(function(data){ console.log(data)})
 
-wrapper()
