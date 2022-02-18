ed39f6f79aa81426b5d4f60209eae8c4b10f63e7
Zenith Zack <faldund@outlook.com>
2022-02-18 16:23:25 -0500

    added camera to the code

--- learningmachines/searcher/static/searcher/bert_method_3d.js
+++ learningmachines/searcher/static/searcher/bert_method_3d.js
@@ -1,8 +1,22 @@
 'use-strict';
 
 async function wrapper(){
-    let num_points = 200;
-    console.log(num_points);
+    let num_points = 200
+    console.log(num_points)
+
+    let width = window.innerWidth
+    let height = window.innerHeight
+    let viz_width = window.innerWidth
+    let aspect = width / height
+
+    let fov = 40
+    let near = 10
+    let far = 7000
+
+    // Setup camera and scene
+    let camera = new THREE.PerspectiveCamera(
+        fov, aspect, near, far
+    )
 }
 
 wrapper()
