63cfc8a477b47655499f1095d53e82546e85b135
Zhaowezra <edgertej@ucmail.uc.edu>
2022-02-24 09:32:00 -0600

    changing annotation mode to upper navbar"

--- learningmachines/searcher/static/searcher/multi-level-lda/multi_vis_annotation.js
+++ learningmachines/searcher/static/searcher/multi-level-lda/multi_vis_annotation.js
@@ -1,14 +1,27 @@
 'use strict'
 
-function handleOnOff(){
-	$('#annotation-off').on("change")
-	$('#annotation-on').(false)
-}
+let annotate_mode = false;
 
-function createAnnotation(){
+$('#annotation-button').on("click", function(e){
+	console.log("clicked")
+	annotate_mode = true
+	if(annotate_mode == true){
+		$('#e-a-b').removeClass("hidden");
+		$('#main-nav').css("background-color", 'grey')
+	}
+})
+
+$('#e-a-b').on("click", function(e){
+	annotate_mode = true
+	$('#e-a-b').addClass("hidden");
+	$('#main-nav').css("background-color", 'white')
+})
+
+
+/*function createAnnotation(){
 	.append("g")
   .attr("class", "annotation-group")
   .call(makeAnnotations)
-}
+}*/
 
 handleOnOff()
