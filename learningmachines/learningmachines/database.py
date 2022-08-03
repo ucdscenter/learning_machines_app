e9c6f79564eb0d9e3b85808e530d4c4213a0d8f0
Thomas Luong <luongcat162002@gmail.com>
2022-08-03 13:14:44 -0400

    Changes to frontend (newly added files)

---
+++ learningmachines/learningmachines/database.py
@@ -0,0 +1,5 @@
+import json
+db_json = open('C:/Users\Admin\learning_machines_app\learningmachines\learningmachines\database.json', 'r')
+data = json.load(db_json)
+db_json.close()
+databases = data
